d77f756ea8a3e3c5683bc0fda89774236d7006db
inhawinter <inhawinter@gmail.com>
2023-03-28 21:48:28 +0900

    week04 main method args v0.2

--- week04/src/MainDemo.java
+++ week04/src/MainDemo.java
@@ -1,17 +1,17 @@
 public class MainDemo {
     // javac MainDemo.java -encoding utf-8
-    // java MainDemo 7 9
+    // java MainDemo 0604074123456
 
-    // or
-    // IntelliJ idea
-    // Run -> Edit Configurations -> Build and run -> Prgram arguments
-
-    // length of array : 2
-    // 16
+    // Female
     public static void main(String[] args) {
-        System.out.println("length of array : " + args.length);
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        System.out.println(a+b);
+        String socialSecurityNumber = args[0];
+        char gender = socialSecurityNumber.charAt(6);
+
+        String genderString = switch (gender){
+            case '1', '3' -> "Male";
+            case '2', '4' -> "Female";
+            default -> "Wrong ssn!";
+        };
+        System.out.println(genderString);
     }
 }

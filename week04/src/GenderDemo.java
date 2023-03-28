50b6c78e38614daec8e1f0404c288af6107928b8
inhawinter <inhawinter@gmail.com>
2023-03-28 19:55:37 +0900

    week04 switch v0.4

--- week04/src/GenderDemo.java
+++ week04/src/GenderDemo.java
@@ -8,17 +8,11 @@ public class GenderDemo {
 
         char gender = socialSecurityNumber.charAt(6);
 
-        switch (gender){
-            case '1':
-            case '3':
-                System.out.println("Male");
-                break;
-            case '2':
-            case '4':
-                System.out.println("Female");
-                break;
-            default:
-                System.out.println("Wrong ssn!");
-        }
+        String genderString = switch (gender){
+            case '1', '3' -> "Male";
+            case '2', '4' -> "Female";
+            default -> "Wrong ssn!";
+        };
+        System.out.println(genderString);
     }
 }
